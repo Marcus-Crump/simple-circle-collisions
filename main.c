6af01e63fd21588115e54143ece67e4ec509d393
Marcus <m.crump@utexas.edu>
2026-06-22 23:14:55 -0500

    updated the program to run with header file I created, and updated it so that it would also initialize the env variable in an if statement. Also cleaned up some syntax

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "collision_objects.h"
 #include "raylib.h"
 
@@ -9,28 +10,28 @@ int main(void)
     const int screenWidth = 1600;
     const int screenHeight = 900;
 
-    Environment env;
+    Environment* env;
+
+    SetTraceLogLevel(LOG_NONE);
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
-	
-    if (init_environment(&env)) { return 1; }
-
+    if (!(env = init_environment())) { return 1; }
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
 
 	// Check for collisions before making changed to the environment
-	if (IsWindowResized()) { set_environment_boundaries(&env); }
+	if (IsWindowResized()) { set_environment_boundaries(env); }
 
         // Update
         //----------------------------------------------------------------------------------
-        if (IsKeyPressed(KEY_EQUAL)) { add_object(&env); }
-        if (IsKeyPressed(KEY_MINUS)) { remove_object(&env); }
+        if (IsKeyPressed(KEY_EQUAL)) { add_object(env); }
+        if (IsKeyPressed(KEY_MINUS)) { remove_object(env); }
 
-	check_collision(&env);
+	check_collision(env);
 	//----------------------------------------------------------------------------------
 
         // Draw
@@ -39,7 +40,7 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-	    draw_environment(&env);
+	    draw_environment(env);
 
             DrawText("Congrats! You created your first window!", 0, 0, 20, LIGHTGRAY);
 
